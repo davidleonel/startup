cea5f6f575ef7ad3c1e65bf38b326511bec3dec3
davidleonel <garcia.david.leonel@gmail.com>
2016-01-17 16:32:10 -0300

    Topic 1: JavaScript Intro and jQuery 1,2,3,4,5,6,7,8,9,10
    10-When clicking the created button and using the input value as q
    parameter and 'track' as type parameter get the response from
    https://api.spotify.com/v1/search. First log the service response in
    Chrome's console and then display tracks into the previously created
    list. Each item must show the track name, artist and duration.
    
    DONE

--- 01-js-intro-and-jquery/javascript.js
+++ 01-js-intro-and-jquery/javascript.js
@@ -3,20 +3,13 @@ $(document).ready(function() {
 	$("#txtName").focus();
 
 	$("#btnName").click(function (){
-		/*
-		$(".answer").load("http://bootcamp.aws.af.cm/welcome/David", function (responseText, textStatus, req) {
 
-			if (textStatus == "error"){
-				$(".hidden").css("color","red");
-	        	return "ERROR!!";
-			}
 
-		});
-		*/
 		var name = document.getElementById("txtName").value;
 
 		$.ajax({
-			url: "http://bootcamp.aws.af.cm/welcome/"+name,
+			url: "http://bootcamp.aws.af.cm/welcome/yourname",
+			data: {name},
 			success: function(result){
 				$(".answer").html(result);
 				$(".answer").css("background-color","yellow");
@@ -30,20 +23,46 @@ $(document).ready(function() {
 	});
 
 
-	$("#btnName").click(function (){
+	$("#btnSearch").click(function (){
 
 		var search = document.getElementById("txtSearch").value;
-
+		var searchType = "track";
 		$.ajax({
-			url: " https://api.spotify.com/v1/"+search,
+			url: "https://api.spotify.com/v1/search",			
+			data: {q: search, type: searchType},
 			success: function(result){
 				console.log(result);
+				if(result){
+					$('.answerSongs').fadeIn("slow");
+	                var len = result.tracks.items.length;
+	                if(len > 0){	                   
+	                    for(var i=0;i<len;i++){
+	                        $("#albums").append("<li> Track Name: "+result.tracks.items[i].name+"</br> Artist: "+result.tracks.items[i].artists[0].name+"</br>Duration: "+result.tracks.items[i].duration_ms+"</li>");
+	                    }
+	                }
+            	}
 			},
+			error: function(){
+				$(".answer").html("ERROR!!!");
+				$("section").css("color","red");
+			},
+
 		});
 
 	});
 
-
-
 	
 });
+
+/* ALBUMS
+if(result){
+					$('.answerSongs').fadeIn("slow");
+	                var len = result.albums.items.length;
+	                if(len > 0){	                   
+	                    for(var i=0;i<len;i++){
+	                        $("#albums").append("<li>"+result.albums.items[i].name+"</li>");
+	                    }
+	                }
+            	}
+
+*/
