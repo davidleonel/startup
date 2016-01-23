bba68d117359753809d050d14263a2db1953d70f
davidleonel <garcia.david.leonel@gmail.com>
2016-01-23 11:53:26 -0300

    Topic 2: Final changes.
    Moved the code around because some methods were not working do to bad
    positioning from inside of the script.

--- 02-oop-inheritance/index.js
+++ 02-oop-inheritance/index.js
@@ -1,11 +1,8 @@
 //----------------------------------
+
 var californication = new Track('californication', 'red hot chili peppers', 3);
 californication.addObserver(trackObserver);
 
-
-extend(DownloadableTrack, Track);
-
-mixinAugment(Track, Social, "share");
 
 //Create artist/ band members
 var anthony = new Artist('Anthony', 'Kiedis', 53);
--- 02-oop-inheritance/script.js
+++ 02-oop-inheritance/script.js
@@ -1,5 +1,36 @@
+//$.extend(Track, DownloadableTrack);
+var extend = function extend(child, parent){
+  var copyOfParent = Object.create(parent.prototype);
+  copyOfParent.constructor = child;
+ 	child.prototype = copyOfParent;	
+};
+
+var mixinAugment = function (target, source){
+    if (arguments[2]) {
+        var index = 2;
+        for (index; index < arguments.length; index += 1 ) {
+            target.prototype[arguments[index]] = source.prototype[arguments[index]];
+        }
+    }
+    else {
+        for (var prop in givingClass.prototype) {
+            if ( !Object.hasOwnProperty.call(receivingClass.prototype, prop) ) {
+                receivingClass.prototype[prop] = givingClass.prototype[prop];
+            }
+        }
+    }
+};
+
+
+//------------------------OBSERVER-------------------
+var Observer  = function Observer(eventCallback){
+    
+    this.eventCallback = eventCallback || [] ;
+};
+
+
 //------------------------OBSERVABLE--------------
-var Observable = function(){
+var Observable = function Observable(){
 	this.observers = [];
 };
 Observable.prototype.addObserver = function(observer){
@@ -19,11 +50,76 @@ Observable.prototype.notify = function(event, track){
     }
 };
 
-//------------------------OBSERVER-------------------
-var Observer  = function Observer(eventCallback){
-    
-    this.eventCallback = eventCallback || [] ;
+
+//----------------------SOCIAL------------
+
+var Social = function(){
 };
+
+Social.prototype.share = function(friendName){
+	this.notify("share", friendName);
+};
+Social.prototype.like = function(){
+	console.log("Liked");
+};
+
+//----------------------Artist------------
+var Artist = function (firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+};
+
+//----------------------------TRACK----------------------
+var Track = (function () {
+
+	function Track (title, artist, duration, artists){
+		this.title = title;
+		this.artist = artist;
+		this.duration = duration;
+		this.artists = artists || [];
+		//this.observable = new Observable(); 
+		Observable.call(this);
+	}
+
+	extend(Track, Observable);
+	mixinAugment(Track, Social, "share");
+
+	Track.prototype.play = function(){
+		this.notify("play", this);
+	};
+	Track.prototype.stop = function(){
+		this.notify("stop", this);
+	},
+	Track.prototype.set = function(attr, value){
+		var temp = attr;
+		this[temp] = value;
+	};
+	Track.prototype.get = function(attr){
+		var temp = attr;	
+		return this[temp];
+	};
+	Track.prototype.addMember = function (artist) {
+        this.artists.push(artist);
+    };
+	
+	return Track;		
+
+})();
+
+
+//----------------------DownloadableTrack------------
+
+var DownloadableTrack = function DownloadableTrack(title, artist, duration){
+	Track.call(this, title, artist, duration);
+};
+
+extend(DownloadableTrack, Track);
+
+DownloadableTrack.prototype.download = function(){
+	this.notify("download", this);
+	};
+
 
 //------------------------TRACKOBSERVER-------------------
 var trackObserver  = new Observer ([
@@ -53,95 +149,3 @@ var trackObserver  = new Observer ([
 		}
 	}
 ]);
-
-
-//----------------------------TRACK----------------------
-var Track = (function () {
-
-	function Track (title, artist, duration, artists){
-		this.title = title;
-		this.artist = artist;
-		this.duration = duration;
-		this.artists = artists || [];
-		this.observable = new Observable(); 
-	}
-	
-	Track.prototype.addObserver = function (observer){
-		this.observable.addObserver(observer)
-	};
-	Track.prototype.play = function(){
-		this.observable.notify("play", this);
-	};
-	Track.prototype.stop = function(){
-		this.observable.notify("stop", this);
-	},
-	Track.prototype.set = function(attr, value){
-		var temp = attr;
-		this[temp] = value;
-	};
-	Track.prototype.get = function(attr){
-		var temp = attr;	
-		return this[temp];
-	};
-	Track.prototype.addMember = function (artist) {
-        this.artists.push(artist);
-    };
-	
-	return Track;		
-
-})();
-
-
-//----------------------DownloadableTrack------------
-
-var DownloadableTrack = function DownloadableTrack (title, artist, duration){
-	Track.call(this, title, artist, duration);
-};
-DownloadableTrack.prototype.download = function(){
-	this.notify("download", this);
-	};
-
-
-//$.extend(Track, DownloadableTrack);
-var extend = function (child, parent){
-	var copyOfParent = Object.create(parent.prototype);
-	copyOfParent.constructor = child;
-    child.prototype = copyOfParent;
-}
-
-
-
-//----------------------SOCIAL------------
-
-var Social = function(){
-};
-
-Social.prototype.share = function(friendName){
-	this.observable.notify("share", friendName);
-};
-Social.prototype.like = function(){
-	console.log("Liked");
-};
-
-var mixinAugment = function (target, source){
-    if (arguments[2]) {
-        var index = 2;
-        for (index; index < arguments.length; index += 1 ) {
-            target.prototype[arguments[index]] = source.prototype[arguments[index]];
-        }
-    }
-    else {
-        for (var prop in givingClass.prototype) {
-            if ( !Object.hasOwnProperty.call(receivingClass.prototype, prop) ) {
-                receivingClass.prototype[prop] = givingClass.prototype[prop];
-            }
-        }
-    }
-}
-
-//----------------------Artist------------
-var Artist = function (firstName, lastName, age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-};
