e8f2adcf36851d32ecc5352303e7a4e3ca009ea5
davidleonel <garcia.david.leonel@gmail.com>
2016-01-25 13:42:23 -0300

    Topic 3: Updated the api.js and index.html
    Updated the files so it can show the artist awards using jquery.

--- 03-Node-npm-modules/api.js
+++ 03-Node-npm-modules/api.js
@@ -9,3 +9,15 @@ jordanSuckley.set('awards', ['X', 'Y', 'Z']);
 droid.set('artist', jordanSuckley);
 
 droid.get('artist').recognice(); //console: Jordan Suckley has the following awards: ...*/
+
+
+showAwards = function () {
+    var $container = $(".content");
+    var $awards = $("<awards>");
+    var $a = droid.get("artist").recognice();
+
+    $awards.append($p);
+    $container.append($awards);
+};
+
+$(document).on("ready", showAwards);
